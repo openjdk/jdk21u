95fb4bceabb70cf6bcdc6ffa55ef3d6345acf289
klaus4 <maciphone2@googlemail.com>
2019-08-14 21:56:31 +0200

    Support FreeBSD < 12.1 aarch64 (#67)
    * support FreeBSD < 12.1

--- src/hotspot/cpu/aarch64/vm_version_aarch64.cpp
+++ src/hotspot/cpu/aarch64/vm_version_aarch64.cpp
@@ -45,6 +45,10 @@
 #define getauxval(hwcap) 0
 #endif
 
+#ifndef HWCAP_ASIMD
+#define HWCAP_ASIMD (1<<1)
+#endif
+
 #ifndef HWCAP_AES
 #define HWCAP_AES   (1<<3)
 #endif
--- src/hotspot/os_cpu/bsd_aarch64/vm_version_bsd_aarch64.cpp
+++ src/hotspot/os_cpu/bsd_aarch64/vm_version_bsd_aarch64.cpp
@@ -32,6 +32,50 @@
 #include <machine/elf.h>
 #endif
 
+#ifndef HWCAP_ASIMD
+#define HWCAP_ASIMD (1<<1)
+#endif
+
+#ifndef HWCAP_AES
+#define HWCAP_AES   (1<<3)
+#endif
+
+#ifndef HWCAP_PMULL
+#define HWCAP_PMULL (1<<4)
+#endif
+
+#ifndef HWCAP_SHA1
+#define HWCAP_SHA1  (1<<5)
+#endif
+
+#ifndef HWCAP_SHA2
+#define HWCAP_SHA2  (1<<6)
+#endif
+
+#ifndef HWCAP_CRC32
+#define HWCAP_CRC32 (1<<7)
+#endif
+
+#ifndef HWCAP_ATOMICS
+#define HWCAP_ATOMICS (1<<8)
+#endif
+
+#ifndef ID_AA64PFR0_AdvSIMD_SHIFT
+#define ID_AA64PFR0_AdvSIMD_SHIFT 20
+#endif
+
+#ifndef ID_AA64PFR0_AdvSIMD
+#define ID_AA64PFR0_AdvSIMD(x) ((x) & (UL(0xf) << ID_AA64PFR0_AdvSIMD_SHIFT))
+#endif
+
+#ifndef ID_AA64PFR0_AdvSIMD_IMPL
+#define ID_AA64PFR0_AdvSIMD_IMPL (UL(0x0) << ID_AA64PFR0_AdvSIMD_SHIFT)
+#endif
+
+#ifndef ID_AA64PFR0_AdvSIMD_HP
+#define ID_AA64PFR0_AdvSIMD_HP (UL(0x1) << ID_AA64PFR0_AdvSIMD_SHIFT)
+#endif
+
 #define	CPU_IMPL_ARM		0x41
 #define	CPU_IMPL_BROADCOM	0x42
 #define	CPU_IMPL_CAVIUM		0x43
@@ -206,11 +250,12 @@ unsigned long VM_Version::os_get_processor_features() {
   if (ID_AA64ISAR0_CRC32(id_aa64isar0) == ID_AA64ISAR0_CRC32_BASE) {
     auxv = auxv | HWCAP_CRC32;
   }
-
+ 
   if (ID_AA64PFR0_AdvSIMD(id_aa64pfr0) == ID_AA64PFR0_AdvSIMD_IMPL || \
       ID_AA64PFR0_AdvSIMD(id_aa64pfr0) == ID_AA64PFR0_AdvSIMD_HP ) {
     auxv = auxv | HWCAP_ASIMD;
   }
-
+	
   return auxv;
 }
+
