4670feb14d4171c7f2617978e4b9ed55a31baddd
Greg Lewis <glewis@eyesbeyond.com>
2019-08-23 22:24:14 -0700

    Fix the header order
    * Include <sys/types.h> before <sys/sysctl.h> per sysctl(3).
    
      Fixes an error compiling this file with gcc8.

--- src/jdk.hotspot.agent/bsd/native/libsaproc/ps_proc.c
+++ src/jdk.hotspot.agent/bsd/native/libsaproc/ps_proc.c
@@ -27,8 +27,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-#include <sys/sysctl.h>
 #include <sys/types.h>
+#include <sys/sysctl.h>
 #include <sys/wait.h>
 #include <sys/ptrace.h>
 #include <sys/param.h>
