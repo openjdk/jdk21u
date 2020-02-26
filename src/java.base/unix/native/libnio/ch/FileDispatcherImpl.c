6b6950d1a9206c3a0094345c3407b12d16dd75be
Greg Lewis <glewis@eyesbeyond.com>
2020-02-25 16:08:10 -0800

    Don't throw IOException if the close(2) failure was due to ECONNRESET
    This seems to be relatively unique to *BSD and causes problems with at
    least Jetty 9.x.
    
    Please see Issue #94 for details.
    
    This doesn't need to be ported to other openjdk versions.  Newer versions
    have changed the way socket channel closure works to ignore an IOException
    on close.  This could potentially be removed if that is ever backported to
    openjdk11
    
    With this change in place no change in Tier 1 internal tests were observed.
    
    Discussed with:	@bsdkurt

--- src/java.base/unix/native/libnio/ch/FileDispatcherImpl.c
+++ src/java.base/unix/native/libnio/ch/FileDispatcherImpl.c
@@ -291,7 +291,7 @@ Java_sun_nio_ch_FileDispatcherImpl_release0(JNIEnv *env, jobject this,
 static void closeFileDescriptor(JNIEnv *env, int fd) {
     if (fd != -1) {
         int result = close(fd);
-        if (result < 0)
+        if (result < 0 && errno != ECONNRESET)
             JNU_ThrowIOExceptionWithLastError(env, "Close failed");
     }
 }
