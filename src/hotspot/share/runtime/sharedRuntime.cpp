32001a762498abdc913574d81657a90573ed91bc
Greg Lewis <glewis@eyesbeyond.com>
2020-04-14 20:09:06 -0700

    Fix build on Clang 10
    Authored by:	Trond.Endrestol@ximalas.info

--- src/hotspot/share/runtime/sharedRuntime.cpp
+++ src/hotspot/share/runtime/sharedRuntime.cpp
@@ -2850,7 +2850,7 @@ void AdapterHandlerLibrary::create_native_wrapper(const methodHandle& method) {
     if (buf != NULL) {
       CodeBuffer buffer(buf);
       double locs_buf[20];
-      buffer.insts()->initialize_shared_locs((relocInfo*)locs_buf, sizeof(locs_buf) / sizeof(relocInfo));
+      buffer.insts()->initialize_shared_locs((relocInfo*)locs_buf, (sizeof(locs_buf)) / (sizeof(relocInfo)));
       MacroAssembler _masm(&buffer);
 
       // Fill in the signature array, for the calling-convention call.
