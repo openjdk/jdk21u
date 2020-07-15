c6cbb3b1e1303f1c89e204e13d9a4495a6bd99a6
Kurt Miller <bsdkurt@gmail.com>
2020-07-15 13:01:21 -0400

    OpenBSD doesn't have RLIMIT_AS or utmpx.h.

--- src/hotspot/os/posix/os_posix.cpp
+++ src/hotspot/os/posix/os_posix.cpp
@@ -49,7 +49,9 @@
 #include <sys/utsname.h>
 #include <time.h>
 #include <unistd.h>
+#ifndef __OpenBSD__
 #include <utmpx.h>
+#endif
 
 // Todo: provide a os::get_max_process_id() or similar. Number of processes
 // may have been configured, can be read more accurately from proc fs etc.
@@ -432,6 +434,7 @@ void os::Posix::print_load_average(outputStream* st) {
   st->cr();
 }
 
+#ifndef __OpenBSD__
 // boot/uptime information;
 // unfortunately it does not work on macOS and Linux because the utx chain has no entry
 // for reboot at least on my test machines
@@ -451,6 +454,7 @@ void os::Posix::print_uptime_info(outputStream* st) {
     os::print_dhm(st, "OS uptime:", (long) (currsec-bootsec));
   }
 }
+#endif
 
 static void print_rlimit(outputStream* st, const char* msg,
                          int resource, bool output_k = false) {
