7a38425ceb311d0060ec756f26a86fc56add644d
Kurt Miller <bsdkurt@gmail.com>
2021-01-04 19:20:51 -0500

    Add missing non static field for *BSD on aarch64

--- src/hotspot/os_cpu/bsd_aarch64/vmStructs_bsd_aarch64.hpp
+++ src/hotspot/os_cpu/bsd_aarch64/vmStructs_bsd_aarch64.hpp
@@ -36,7 +36,8 @@
   /* Threads (NOTE: incomplete) */                                                                                                   \
   /******************************/                                                                                                   \
   nonstatic_field(OSThread,                      _thread_id,                                      OSThread::thread_id_t)             \
-  nonstatic_field(OSThread,                      _pthread_id,                                     pthread_t)
+  nonstatic_field(OSThread,                      _pthread_id,                                     pthread_t)                         \
+  nonstatic_field(OSThread,                      _unique_thread_id,                               uint64_t)
 
 
 #define VM_TYPES_OS_CPU(declare_type, declare_toplevel_type, declare_oop_type, declare_integer_type, declare_unsigned_integer_type, declare_c1_toplevel_type, declare_c2_type, declare_c2_toplevel_type) \
