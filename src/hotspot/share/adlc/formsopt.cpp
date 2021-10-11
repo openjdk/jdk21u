ea2723bb26e0a79b6c1116d6ef8fa73c21d1e6f4
Greg Lewis <glewis@eyesbeyond.com>
2021-10-10 21:02:11 -0700

    Fix build on FreeBSD/aarch64
    * Fully initialize FrameForm.  Corrects adlc segfault when malloc
      junk options are set.
    
    Authored by:	Mikael Urankar <mikael@FreeBSD.org>

--- src/hotspot/share/adlc/formsopt.cpp
+++ src/hotspot/share/adlc/formsopt.cpp
@@ -436,15 +436,20 @@ void AllocClass::output(FILE *fp) {       // Write info to output files
 //==============================Frame Handling=================================
 //------------------------------FrameForm--------------------------------------
 FrameForm::FrameForm() {
+  _sync_stack_slots = NULL;
+  _inline_cache_reg = NULL;
+  _interpreter_frame_pointer_reg = NULL;
+  _cisc_spilling_operand_name = NULL;
   _frame_pointer = NULL;
   _c_frame_pointer = NULL;
   _alignment = NULL;
+  _return_addr_loc = false;
+  _c_return_addr_loc = false;
   _return_addr = NULL;
   _c_return_addr = NULL;
   _varargs_C_out_slots_killed = NULL;
   _return_value = NULL;
   _c_return_value = NULL;
-  _interpreter_frame_pointer_reg = NULL;
 }
 
 FrameForm::~FrameForm() {
