cab3ef2db0fef2d3e2bad9ab449fb0e225dd3ee4
airsquared <36649395+airsquared@users.noreply.github.com>
2023-08-31 19:55:29 -0700

    Update LauncherData.java

--- src/jdk.jpackage/share/classes/jdk/jpackage/internal/LauncherData.java
+++ src/jdk.jpackage/share/classes/jdk/jpackage/internal/LauncherData.java
@@ -24,8 +24,6 @@
  */
 package jdk.jpackage.internal;
 
-import jdk.internal.util.OperatingSystem;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
@@ -60,10 +58,6 @@ final class LauncherData {
 
     String qualifiedClassName() {
         return qualifiedClassName;
-    }
-
-    boolean isClassNameFromMainJar() {
-        return jarMainClass != null;
     }
 
     String packageName() {
@@ -105,7 +99,7 @@ final class LauncherData {
     }
 
     private void verifyIsModular(boolean isModular) {
-        if ((moduleInfo != null) != isModular) {
+        if ((moduleInfo == null) == isModular) {
             throw new IllegalStateException();
         }
     }
@@ -215,7 +209,6 @@ final class LauncherData {
                 if (attrs != null) {
                     launcherData.qualifiedClassName = attrs.getValue(
                             Attributes.Name.MAIN_CLASS);
-                    launcherData.jarMainClass = launcherData.qualifiedClassName;
                 }
             }
         }
@@ -266,14 +259,10 @@ final class LauncherData {
     private static String getStringParam(Map<String, ? super Object> params,
             String paramName) {
         Optional<Object> value = Optional.ofNullable(params.get(paramName));
-        if (value.isPresent()) {
-            return value.get().toString();
-        }
-        return null;
+        return value.map(Object::toString).orElse(null);
     }
 
-    private static <T> T getPathParam(Map<String, ? super Object> params,
-            String paramName, Supplier<T> func) throws ConfigException {
+    private static <T> T getPathParam(String paramName, Supplier<T> func) throws ConfigException {
         try {
             return func.get();
         } catch (InvalidPathException ex) {
@@ -285,7 +274,7 @@ final class LauncherData {
 
     private static Path getPathParam(Map<String, ? super Object> params,
             String paramName) throws ConfigException {
-        return getPathParam(params, paramName, () -> {
+        return getPathParam(paramName, () -> {
             String value = getStringParam(params, paramName);
             Path result = null;
             if (value != null) {
@@ -304,7 +293,7 @@ final class LauncherData {
             runtimePath = runtimePath.resolve("lib");
             modulePath = Stream.of(modulePath, List.of(runtimePath))
                     .flatMap(List::stream)
-                    .collect(Collectors.toUnmodifiableList());
+                    .toList();
         }
 
         return modulePath;
@@ -312,17 +301,12 @@ final class LauncherData {
 
     private static List<Path> getPathListParameter(String paramName,
             Map<String, ? super Object> params) throws ConfigException {
-        return getPathParam(params, paramName, () -> {
-            String value = (String) params.get(paramName);
-            return (value == null) ? List.of() :
-                    List.of(value.split(File.pathSeparator)).stream()
-                    .map(Path::of)
-                    .collect(Collectors.toUnmodifiableList());
-        });
+        return getPathParam(paramName, () ->
+                params.get(paramName) instanceof String value ?
+                        Stream.of(value.split(File.pathSeparator)).map(Path::of).toList() : List.of());
     }
 
     private String qualifiedClassName;
-    private String jarMainClass;
     private Path mainJarName;
     private List<Path> classPath;
     private List<Path> modulePath;
@@ -360,7 +344,7 @@ final class LauncherData {
             // of `release` file.
 
             final Path releaseFile;
-            if (!OperatingSystem.isMacOS()) {
+            if (!Platform.isMac()) {
                 releaseFile = cookedRuntime.resolve("release");
             } else {
                 // On Mac `cookedRuntime` can be runtime root or runtime home.
