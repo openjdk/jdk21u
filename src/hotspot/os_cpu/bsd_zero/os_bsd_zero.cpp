437dabc32fb02e7153842b28871b2ca2dbf24ca4
Greg Lewis <glewis@eyesbeyond.com>
2023-10-20 23:09:55 -0700

    Fix zero build
    Fix merge botch

--- src/hotspot/os_cpu/bsd_zero/os_bsd_zero.cpp
+++ src/hotspot/os_cpu/bsd_zero/os_bsd_zero.cpp
@@ -352,7 +352,8 @@ void os::print_tos_pc(outputStream *st, const void* ucVoid) {
   st->cr();
 }
 
-void os::print_register_info(outputStream *st, const void* ucVoid) {
+
+void os::print_register_info(outputStream *st, const void *context, int& continuation) {
   st->print_cr("No register info.");
 }
 
