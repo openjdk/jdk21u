5bb84449cf325831cf6541306cd3968f37634208
Alexandros Antonakakis <antonakakis1@gmail.com>
2024-02-24 19:45:02 +0200

    update

--- src/hotspot/share/runtime/sharedRuntime.cpp
+++ src/hotspot/share/runtime/sharedRuntime.cpp
@@ -196,6 +196,7 @@ void lock_or_unlock(Method *m) {
 }
 
 JRT_LEAF(void, SharedRuntime::monitor_invoke(Method *m))
+  ResourceMark rm;
   const char *name = m->name_and_sig_as_C_string();
 
   if (strstr(name, "java.util.concurrent.locks.ReentrantLock")) {
